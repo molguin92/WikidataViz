9fdafe3899de297888b08891ec90741a7647121a
Manuel Olguin <molguin@dcc.uchile.cl>
2016-12-09 18:22:27 -0300

    fix InfoView import

--- wikidataviz/__init__.py
+++ wikidataviz/__init__.py
@@ -5,7 +5,7 @@ from flask_restful import Api
 app = Flask(__name__)
 api = Api(app)
 
-from wikidataviz.views.gview import GraphView, InfoView
+from wikidataviz.views.gview import GraphView
 
 api.add_resource(GraphView, '/entity')
 
